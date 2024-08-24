3bbd31ba01ea1c7aecbcf88bd01987cb95300930
pokotaro.eth <kentarou_masuda@ruckplus-tech.io>
2024-08-25 00:14:33 +0900

    add client functions

--- src/lib/greenfield/bucket.ts
+++ src/lib/greenfield/bucket.ts
@@ -1,4 +1,19 @@
 import { client, getRandomSp, forEach } from '.'
+import type { GetUserBucketsRequest } from '@bnb-chain/greenfield-js-sdk'
+import type { MsgCreateBucket, MsgDeleteBucket } from '@bnb-chain/greenfield-cosmos-types/greenfield/storage/tx'
+
+
+export const createBucket = async (params: MsgCreateBucket) => {
+  return await client.bucket.createBucket(params)
+}
+
+export const deleteBucket = async (params: MsgDeleteBucket) => {
+  return await client.bucket.deleteBucket(params)
+}
+
+export const listBuckets = async (configParam: GetUserBucketsRequest) => {
+  return await client.bucket.listBuckets(configParam)
+}
 
 export const getBucketList = async (address: string) => {
   const endpoint = await getRandomSp()
